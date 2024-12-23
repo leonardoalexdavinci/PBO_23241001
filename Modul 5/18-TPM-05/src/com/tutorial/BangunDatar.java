3bfcd284aebe5dd056e877ecccb3d64b25bbda53
leonardoalexdavinci <145946853+leonardoalexdavinci@users.noreply.github.com>
2024-12-23 11:16:06 +0800

    Add files via upload

---
+++ Modul 5/18-TPM-05/src/com/tutorial/BangunDatar.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+// Super class
+public class BangunDatar {
+
+    public float luas() {
+        return 0;
+    }
+    
+    public float keliling() {
+        return 0;
+    }
+}
