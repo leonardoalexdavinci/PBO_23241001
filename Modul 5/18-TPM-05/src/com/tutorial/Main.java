0dbfb05d229bf6903e4f03eb78ee65dd8a25b266
leonardoalexdavinci <145946853+leonardoalexdavinci@users.noreply.github.com>
2024-12-23 11:17:37 +0800

    Update Main.java

--- Modul 5/18-TPM-05/src/com/tutorial/Main.java
+++ Modul 5/18-TPM-05/src/com/tutorial/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         
         // objek
-        Persegi persegi = new Persegi(6);
+        Persegi persegi = new Persegi(8);
         System.out.println("Luas Persegi : " + persegi.luas() + "\nKeliling Persegi : " + persegi.keliling());
 
-        PersegiPanjang psgpnjng = new PersegiPanjang(4, 8);
+        PersegiPanjang psgpnjng = new PersegiPanjang(6, 10);
         System.out.println("Luas Persegi Panjang : " + psgpnjng.luas() + "\nKeliling Persegi Panjang : " + psgpnjng.keliling());
 
-        Lingkaran lingkaran = new Lingkaran(6);
+        Lingkaran lingkaran = new Lingkaran(8);
         System.out.println("Luas Lingkaran : " + lingkaran.luas() + "\nKeliling Lingkaran : " + lingkaran.keliling());
 
-        Segitiga segitiga = new Segitiga(5, 5);
+        Segitiga segitiga = new Segitiga(7, 7);
         System.out.println("Luas Segitiga : " + segitiga.luas() + "\nKeliling Segitiga : " + segitiga.keliling());
 
     }
